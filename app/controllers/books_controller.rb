809a88166f70489db84ffbfd21c6e5c81a5e722f
lou-GH <117365073+lou-GH@users.noreply.github.com>
2022-11-19 16:15:27 +0000

    add 新規・indexレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -10,4 +18,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
